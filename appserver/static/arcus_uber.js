a8ce5007abc0a1fd2ae3ce68cace55a5d6cab8ee
Joshua McQueen <10521798+joshmcqueen@users.noreply.github.com>
2019-02-13 17:31:47 -0800

    Initial commit

---
+++ appserver/static/arcus_uber.js
@@ -0,0 +1,10 @@
+require([
+    'jquery',
+    'splunkjs/mvc/simplexml/ready!'
+], function ($) {
+    $(document).ready(function () {
+        console.log("Loading External CSS");
+        $('head').append('<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">');
+        $('head').append('<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.7.2/css/brands.css">');
+    });
+});
